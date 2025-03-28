c2c6735401714fb9c95973c88bf3862a1f46c7f2
Frederico Carvalho <frederico.ferreira.carvalho@gmail.com>
2025-03-28 16:04:43 +0000

    Mygame and enemy created

---
+++ MyGame/Enemy.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace MyRPG
+{
+    public class Enemy
+    {
+        private string name;
+        private float health;
+        private float shield;
+        private static int totalPowerUpsCollected;
+
+        static Enemy()
+        {
+            totalPowerUpsCollected = 0;
+        }
+
+        public Enemy(string name)
+        {
+            SetName(name);
+            health = 100;
+            shield = 0;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public float GetHealth()
+        {
+            return health;
+        }
+
+        public float GetShield()
+        {
+            return shield;
+        }
+
+        public static int GetTotalPowerUpsCollected()
+        {
+            return totalPowerUpsCollected;
+        }
+
+        public void SetName(string newName)
+        {
+            name = newName.Substring(0, Math.Min(8, newName.Length));
+        }
+
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+    }
+}
